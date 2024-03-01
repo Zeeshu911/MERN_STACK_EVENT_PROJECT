b53fbb41dbe6d8426a6624c2439c787dbb72b550
Muhammad Zeeshan Khan <117258902+Zeeshu911@users.noreply.github.com>
2024-03-01 10:52:42 +0500

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -19,13 +19,6 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.json({
-    success: true,
-    message: "HABIBI COME TO DUBAI",
-  });
-});
-
 app.use("/api/v1/message", messageRouter);
 
 dbConnection();
