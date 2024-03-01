4f187461a674e8dc32c9407311d92c6201a284f0
Muhammad Zeeshan Khan <117258902+Zeeshu911@users.noreply.github.com>
2024-03-01 10:52:13 +0500

    Update Contact.jsx

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -12,7 +12,7 @@ const Contact = () => {
     e.preventDefault();
     await axios
       .post(
-        "https://successful-threads-fish.cyclic.app/api/v1/message/send",
+        "http://localhost:4000/api/v1/message/send",
         {
           name,
           email,
